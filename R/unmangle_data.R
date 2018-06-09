87ae0203f891bd76514d37b96d3b5035d5fc6176
Pieter van den Berg <pvdberg@gmail.com>
2018-06-09 10:07:05 +0000

    bring data in appr normal form

---
+++ R/unmangle_data.R
@@ -0,0 +1,19 @@
+# bring data in normal form
+
+# txt files to fst
+
+files <- list.files('../../data/Lung/', full.names = T, pattern = 'txt$')
+files <- set_names(files, str_replace(basename(files), '.txt',''))
+files_fst <- map(files,str_c,'.fst')
+# walk2(files,files_fst,~fst::write_fst(read_tsv(..1),path=..2))
+
+## Gene expression data
+
+Lung_GeneExpression <- read_fst(files_fst$Lung_GeneExpression) %>% as_data_frame
+Lung_GeneExpression_transposed <- Lung_GeneExpression %>% 
+    gather(SampleID, value = gene_expression, starts_with("TCGA")) %>% select(Gene,Chr,gene_expression,SampleID) %>%
+    tidyr::unite(gene_chr, Gene,Chr) %>% 
+    distinct(SampleID, gene_chr,.keep_all=TRUE) %>% 
+    spread(gene_chr,gene_expression) 
+write_fst(Lung_GeneExpression_transposed,'../data/intermediates/Lung_GeneExpression_normal.fst')
+
