7d8ac7e4226234813b1162c8a44dd8a78c3373ab
Pieter van den Berg <pvdberg@gmail.com>
2018-06-09 10:53:28 +0000

    unmangle data

--- R/unmangle_data.R
+++ R/unmangle_data.R
@@ -1,10 +1,13 @@
 # bring data in normal form
 require(purrr)
 require(fst)
+require(tibble)
+require(dplyr)
+require(tidyr)
 require(stringr)
 # txt files to fst
 
-files <- list.files('Z:/startset/Lung', full.names = T, pattern = 'txt$')
+files <- list.files('../../data/Lung', full.names = T, pattern = 'txt$')
 files <- set_names(files, str_replace(basename(files), '.txt',''))
 files_fst <- map(files,str_c,'.fst')
 # walk2(files,files_fst,~fst::write_fst(read_tsv(..1),path=..2))
@@ -13,9 +16,28 @@ files_fst <- map(files,str_c,'.fst')
 
 Lung_GeneExpression <- read_fst(files_fst$Lung_GeneExpression) %>% as_data_frame
 Lung_GeneExpression_transposed <- Lung_GeneExpression %>% 
-    gather(SampleID, value = gene_expression, starts_with("TCGA")) %>% select(Gene,Chr,gene_expression,SampleID) %>%
-    tidyr::unite(gene_chr, Gene,Chr) %>% 
-    distinct(SampleID, gene_chr,.keep_all=TRUE) %>% 
-    spread(gene_chr,gene_expression) 
-write_fst(Lung_GeneExpression_transposed,'Z:/intermediates/Lung_GeneExpression_normal.fst')
+  gather(SampleID, value = gene_expression, starts_with("TCGA")) %>% select(Gene,Chr,gene_expression,SampleID) %>%
+  tidyr::unite(gene_chr, Gene,Chr) %>% 
+  distinct(SampleID, gene_chr,.keep_all=TRUE) %>% Lung_Mutation
+  spread(gene_chr,gene_expression) 
+write_fst(Lung_GeneExpression_transposed,'../../data/intermediates/Lung_GeneExpression_normal.fst')
 
+## CNV data
+
+Lung_CNV <- read_fst(files_fst$Lung_CNV) %>% as_data_frame
+Lung_CNV_transposed <- Lung_CNV %>% 
+  gather(SampleID, value = cnv, starts_with("TCGA")) %>% select(Gene,Chr,cnv,SampleID) %>%
+  tidyr::unite(gene_chr, Gene,Chr) %>% 
+  distinct(SampleID, gene_chr,.keep_all=TRUE) %>% 
+  spread(gene_chr,cnv) 
+write_fst(Lung_CNV_transposed,'../../data/intermediates/Lung_CNV_transposed.fst')
+
+## mutation
+
+Lung_Mutation <- read_fst(files_fst$Lung_Mutation) %>% as_data_frame
+Lung_Mutation_transposed <- Lung_Mutation %>% rename(SampleID = Sample_ID) %>% 
+  gather(SampleID, value = Effect, starts_with("TCGA")) %>% select(Gene,Chr,Effect,SampleID) %>%
+  tidyr::unite(gene_chr, Gene,Chr) %>% 
+  distinct(SampleID, gene_chr,.keep_all=TRUE) %>% 
+  spread(gene_chr,Effect) 
+write_fst(Lung_Mutation_transposed,'../../data/intermediates/Lung_Mutation_transposed.fst')
